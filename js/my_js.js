d2b0500d599b38b20d44f17dd69b98353eb0d768
imcgeek <vkr1994@gmail.com>
2018-02-12 03:10:23 +0530

    Reminder Functionality Added

--- js/my_js.js
+++ js/my_js.js
@@ -17,3 +17,13 @@ function div_hide(){
   document.getElementById('calendar').style.visibility = "visible";
 document.getElementById('abc').style.display = "none";
 }
+
+function div_show_reminder() {
+document.getElementById('reminder').style.display = "block";
+document.getElementById('calendar').style.visibility = "hidden";
+}
+//Function to Hide Popup
+function div_hide_reminder(){
+  document.getElementById('calendar').style.visibility = "visible";
+document.getElementById('reminder').style.display = "none";
+}
